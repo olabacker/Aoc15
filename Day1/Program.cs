6cf224b2c276a9971f3fd5a150ea6406bcd5bf37
Ola Bäcker <olabacker@gmail.com>
2019-11-11 18:08:48 +0100

    day1 complete

---
+++ Day1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Properties.Resources.TextFile1;
+
+            char[] chars = input.ToCharArray();
+
+            int elevation = chars.Count(c => c == '(') - chars.Count(c => c == ')');
+
+            Console.WriteLine(elevation);
+
+            int currentElevation = 0;
+            int firstBasementInstruction = -1;
+
+            for(int i = 0; i < chars.Length; i++)
+            {
+                char c = chars[i];
+
+                if(c == '(')
+                {
+                    currentElevation++;
+                }
+                else
+                {
+                    currentElevation--;
+                }
+
+                if(currentElevation == -1)
+                {
+                    firstBasementInstruction = i + 1;
+                    break;
+                }
+            }
+
+            Console.WriteLine(firstBasementInstruction);
+
+            Console.ReadLine();
+
+        }
+    }
+}
