b1d04e6de6d43d0763a9e932b1ca8b2e871b40af
Ola Bäcker <olabacker@gmail.com>
2019-11-16 22:12:19 +0100

    day 7 complete

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day7
 {
     class Program
     {
+        private static readonly Dictionary<string, ushort> cache = new Dictionary<string, ushort>();
+        private static readonly Dictionary<string, string> wires = Properties.Resources.aoc7.Split(Environment.NewLine).ToDictionary(k => k.Split(' ').Last());
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var value = Recursive("a");
+            Console.WriteLine(value);
+            cache.Clear();
+            cache["b"] = value;
+            Console.WriteLine(Recursive("a"));
+        }
+
+        private static ushort Recursive(string s)
+        {
+            ushort temp;
+            if (ushort.TryParse(s, out temp) || cache.TryGetValue(s, out temp)) return temp;
+            var parsed = wires[s].Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            switch (parsed.Reverse().Skip(3).FirstOrDefault()?.ToUpperInvariant())
+            {
+                case "AND": return cache[parsed.Last()] = (ushort)(Recursive(parsed[0]) & Recursive(parsed[2]));
+                case "OR": return cache[parsed.Last()] = (ushort)(Recursive(parsed[0]) | Recursive(parsed[2]));
+                case "LSHIFT": return cache[parsed.Last()] = (ushort)(Recursive(parsed[0]) << Recursive(parsed[2]));
+                case "RSHIFT": return cache[parsed.Last()] = (ushort)(Recursive(parsed[0]) >> Recursive(parsed[2]));
+                case "NOT": return cache[parsed.Last()] = (ushort)~Recursive(parsed[1]);
+                default: return cache[parsed.Last()] = Recursive(parsed[0]);
+            }
         }
     }
 }
